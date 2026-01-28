ae0ae941fa9ca65f8e39497ceaefdffc53733f3c
bharathsindhe2003 <bharath.sindhe@tantragyaansolutions.com>
2026-01-28 12:38:11 +0530

    Use hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import Login from "./Pages/Login";
 import Dashboard from "./Pages/Dashboard";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 // import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <>
       <ToastContainer />
-      <Router basename="/ICU-Clinical-Trials-Dashboard">
+      <Router>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
